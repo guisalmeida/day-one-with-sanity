4dc2562ba8d7c0905b6c970efc2522969e595da7
guisalmeida <guisalmeida.dev@gmail.com>
2024-08-01 18:15:53 -0300

    chore: add studio

---
+++ studio/schemaTypes/index.ts
@@ -0,0 +1,9 @@
+import { eventType } from "./eventType";
+import { venueType } from "./venueType";
+import { artistType } from "./artistType";
+
+export const schemaTypes = [
+    eventType,
+    venueType,
+    artistType
+]
